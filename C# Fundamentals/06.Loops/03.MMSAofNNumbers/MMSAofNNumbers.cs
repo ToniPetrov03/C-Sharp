30f5be6123b774d8d41b6e1a1b561fa00e543acc
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-06 19:34:44 +0200

    MMSA of n numbers

--- C# Fundamentals/06.Loops/03.MMSAofNNumbers/MMSAofNNumbers.cs
+++ C# Fundamentals/06.Loops/03.MMSAofNNumbers/MMSAofNNumbers.cs
@@ -10,7 +10,24 @@ namespace _03.MMSAofNNumbers
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            double min = double.MaxValue;
+            double max = double.MinValue;
+            double sum = 0;
 
+            for (int i = 0; i < n; i++)
+            {
+                double a = double.Parse(Console.ReadLine());
+                if (a < min)
+                    min = a;
+                if (a > max)
+                    max = a;
+                sum += a;
+            }
+            Console.WriteLine("min={0:F2}", min);
+            Console.WriteLine("max={0:F2}", max);
+            Console.WriteLine("sum={0:F2}", sum);
+            Console.WriteLine("avg={0:F2}", sum/n);
         }
     }
 }
