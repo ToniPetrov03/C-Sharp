a02bddf766a27da631213d4a9a20c8b781238f85
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-07 00:33:36 +0200

    Print deck

--- C# Fundamentals/06.Loops/04.PrintDeck/PrintDeck.cs
+++ C# Fundamentals/06.Loops/04.PrintDeck/PrintDeck.cs
@@ -10,6 +10,29 @@ namespace _04.PrintDeck
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> dict = new Dictionary<string, int>
+            {
+                { "2", 2 },
+                { "3", 3 },
+                { "4", 4 },
+                { "5", 5 },
+                { "6", 6 },
+                { "7", 7 },
+                { "8", 8 },
+                { "9", 9 },
+                { "10", 10 },
+                { "J", 11 },
+                { "Q", 12 },
+                { "K", 13 },
+                { "A", 14 }
+            };
+            string card = Console.ReadLine();
+
+            for (int i = 2; i <= dict[card]; i++)
+            {
+                string a = dict.FirstOrDefault(x => x.Value == i).Key;
+                Console.WriteLine(a + " of spades, " + a + " of clubs, " + a + " of hearts, " + a + " of diamonds");
+            }
         }
     }
 }
