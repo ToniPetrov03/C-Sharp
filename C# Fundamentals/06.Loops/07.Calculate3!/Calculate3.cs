63c8637f3b8b880d424d787f0c05d39977422218
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-07 14:29:42 +0200

    Calculate!

--- C# Fundamentals/06.Loops/07.Calculate3!/Calculate3.cs
+++ C# Fundamentals/06.Loops/07.Calculate3!/Calculate3.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Numerics;
 namespace _07.Calculate3_
 {
     class Calculate3
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+            BigInteger result1 = n - k;
+
+            for (int i = k + 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            for (int i = 1; i < n - k; i++)
+            {
+                result1 *= (n - k) - i;
+            }
+            Console.WriteLine(result / result1);
         }
     }
 }
