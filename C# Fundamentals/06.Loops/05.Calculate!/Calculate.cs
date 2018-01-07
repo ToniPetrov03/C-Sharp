81bfbda97ba4e8f2a7002eb9cf4924a9418f413d
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-07 12:03:17 +0200

    Calculate!

--- C# Fundamentals/06.Loops/05.Calculate!/Calculate.cs
+++ C# Fundamentals/06.Loops/05.Calculate!/Calculate.cs
@@ -10,6 +10,21 @@ namespace _05.Calculate_
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+            double sum = 0;
+            double exponential = 1;
+            for (int i = 0; i <= n; i++)
+            {
+                double factoriel = 1;
+                for (int j = 2; j <= i; j++)
+                {
+                    factoriel *= j;
+                }
+                sum += (factoriel / exponential);
+                exponential = exponential * x;
+            }
+            Console.WriteLine("{0:F5}", (sum));
         }
     }
 }
