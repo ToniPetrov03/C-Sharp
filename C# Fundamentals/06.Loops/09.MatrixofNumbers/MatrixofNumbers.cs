12860641a1a523e2544a771f981b07161f748d1b
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-08 22:26:21 +0200

    Matrix of number

--- C# Fundamentals/06.Loops/09.MatrixofNumbers/MatrixofNumbers.cs
+++ C# Fundamentals/06.Loops/09.MatrixofNumbers/MatrixofNumbers.cs
@@ -10,6 +10,16 @@ namespace _09.MatrixofNumbers
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < i + 1 + n; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
