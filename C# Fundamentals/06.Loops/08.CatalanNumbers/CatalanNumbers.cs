8eef07fe9c41b8c21f1606f6cf634c388bfd5723
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-08 22:03:46 +0200

    Catalan numbers

--- C# Fundamentals/06.Loops/08.CatalanNumbers/CatalanNumbers.cs
+++ C# Fundamentals/06.Loops/08.CatalanNumbers/CatalanNumbers.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace _08.CatalanNumbers
 {
@@ -10,6 +7,25 @@ namespace _08.CatalanNumbers
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+            BigInteger result1 = 1;
+            BigInteger result2 = 1;
+
+            for (BigInteger i = 1; i <= 2 * n; i++)
+            {
+                result *= i;
+            }
+            for (BigInteger i = 1; i <= n + 1; i++)
+            {
+                result1 *= i;
+            }
+            for (BigInteger i = 1; i <= n; i++)
+            {
+                result2 *= i;
+            }
+
+            Console.WriteLine(result / (result1 * result2));
         }
     }
 }
