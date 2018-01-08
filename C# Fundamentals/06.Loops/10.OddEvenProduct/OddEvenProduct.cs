130d39e10bd932ce2307dddbc1935f6760f654ae
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-08 22:40:44 +0200

    Commit before sleep

--- C# Fundamentals/06.Loops/10.OddEvenProduct/OddEvenProduct.cs
+++ C# Fundamentals/06.Loops/10.OddEvenProduct/OddEvenProduct.cs
@@ -11,7 +11,6 @@ namespace _10.OddEvenProduct
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-
             for (int i = 1; i <= n; i++)
             {
                 if (i % 2 == 0)
