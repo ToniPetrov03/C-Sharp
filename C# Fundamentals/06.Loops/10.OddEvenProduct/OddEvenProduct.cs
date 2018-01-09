4ac3cf0087e02c7336a62a80833ada5868268aa7
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-09 22:15:09 +0200

    Exams

--- C# Fundamentals/06.Loops/10.OddEvenProduct/OddEvenProduct.cs
+++ C# Fundamentals/06.Loops/10.OddEvenProduct/OddEvenProduct.cs
@@ -11,16 +11,27 @@ namespace _10.OddEvenProduct
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int evenProduct = 1;
+            int oddProduct = 1;
             for (int i = 1; i <= n; i++)
             {
                 if (i % 2 == 0)
                 {
-
+                    evenProduct *= numbers[i - 1];
                 }
                 else
                 {
-
+                    oddProduct *= numbers[i - 1];
                 }
+            }
+            if (evenProduct==oddProduct)
+            {
+                Console.WriteLine("yes "+ evenProduct);
+            }
+            else
+            {
+                Console.WriteLine("no " + oddProduct + " " + evenProduct);
             }
         }
     }
--- C# Fundamentals/06.Loops/11. Binary toDecimal/BinarytoDecimal.cs
+++ C# Fundamentals/06.Loops/11. Binary toDecimal/BinarytoDecimal.cs
@@ -10,6 +10,18 @@ namespace _11.Binary_toDecimal
     {
         static void Main(string[] args)
         {
+            string binary = Console.ReadLine();
+            long dec = 0;
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (binary[binary.Length - i - 1] == '0')                                                                                                                                
+                {
+                    continue;
+                }
+                dec += (long)Math.Pow(2, i);
+            }
+            Console.WriteLine(dec);
         }
     }
 }
