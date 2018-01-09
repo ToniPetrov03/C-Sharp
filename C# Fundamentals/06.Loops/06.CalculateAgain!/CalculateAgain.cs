9c8dfe51b9b0f3cbffbf4d5613d59141b8b95f40
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-09 22:28:01 +0200

    Calculate again!

--- C# Fundamentals/06.Loops/06.CalculateAgain!/CalculateAgain.cs
+++ C# Fundamentals/06.Loops/06.CalculateAgain!/CalculateAgain.cs
@@ -10,6 +10,7 @@ namespace _06.CalculateAgain_
             int n = int.Parse(Console.ReadLine());
             int k = int.Parse(Console.ReadLine());
             BigInteger result = 1;
+
             for (int i = k + 1; i <= n; i++)
             {
                 result *= i;
