4b4c8ea03d2751fabe58babc42b57e0d599d4b3b
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-09 22:31:16 +0200

    Number from 1 to N!

--- C# Fundamentals/06.Loops/01.NumbersFrom1toN/NumbersFrom1toN.cs
+++ C# Fundamentals/06.Loops/01.NumbersFrom1toN/NumbersFrom1toN.cs
@@ -7,7 +7,6 @@ namespace _01.NumbersFrom1toN
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-
             for (int i = 1; i <= n; i++)
             {
                 Console.Write(i + " ");
