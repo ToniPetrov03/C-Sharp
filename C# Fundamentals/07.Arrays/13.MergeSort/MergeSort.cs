f52d25328ed7292a6faa86e9d168ac9ae4d322d2
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-10 17:57:32 +0200

    _

--- C# Fundamentals/07.Arrays/13.MergeSort/MergeSort.cs
+++ C# Fundamentals/07.Arrays/13.MergeSort/MergeSort.cs
@@ -10,6 +10,18 @@ namespace _13.MergeSort
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
         }
     }
 }
