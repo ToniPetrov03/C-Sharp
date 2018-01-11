4a7c84cf2b9bfa81223371d2f6a05b94779d332f
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-11 20:23:14 +0200

    Decimal to binary

--- C# Fundamentals/06.Loops/12.DecimaltoBinary/DecimaltoBinary.cs
+++ C# Fundamentals/06.Loops/12.DecimaltoBinary/DecimaltoBinary.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _12.DecimaltoBinary
+namespace _01.Decimal_to_Binary
 {
-    class DecimaltoBinary
+    class DecimalToBinary
     {
         static void Main(string[] args)
         {
+            int a = int.Parse(Console.ReadLine());
+            int b;
+            string result = string.Empty;
+
+            while (a > 0)
+            {
+                b = a % 2;
+                a /= 2;
+                result = b.ToString() + result;
+            }
+            Console.WriteLine(result);
         }
     }
 }
