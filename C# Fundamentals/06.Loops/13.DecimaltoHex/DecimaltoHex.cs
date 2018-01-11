983fddf593cff48e9c0e95676cd1ff9cc10137f2
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-11 22:03:05 +0200

    GCD

--- C# Fundamentals/06.Loops/13.DecimaltoHex/DecimaltoHex.cs
+++ C# Fundamentals/06.Loops/13.DecimaltoHex/DecimaltoHex.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _13.DecimaltoHex
 {
@@ -10,6 +6,7 @@ namespace _13.DecimaltoHex
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
--- C# Fundamentals/06.Loops/15.GCD/GCD.cs
+++ C# Fundamentals/06.Loops/15.GCD/GCD.cs
@@ -10,6 +10,29 @@ namespace _15.GCD
     {
         static void Main(string[] args)
         {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = input[0];
+            int b = input[1];
+            int x = 0;
+            int n = 0;
+
+            if (a < b)
+            {
+                n = a;
+            }
+            else
+            {
+                n = b;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    x = i;
+                }
+            }
+            Console.WriteLine(x);
         }
     }
 }
