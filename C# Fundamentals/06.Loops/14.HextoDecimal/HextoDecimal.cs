fb0ee0bc452fb0ebdecf7e8c749f7e819eefe08b
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-11 23:01:56 +0200

    Hex to decimal

--- C# Fundamentals/06.Loops/14.HextoDecimal/HextoDecimal.cs
+++ C# Fundamentals/06.Loops/14.HextoDecimal/HextoDecimal.cs
@@ -10,6 +10,25 @@ namespace _14.HextoDecimal
     {
         static void Main(string[] args)
         {
+            string hex = Console.ReadLine();
+            long dec = 0;
+
+            for (int i = 0; i < hex.Length; i++)
+            {
+                long number = 0;
+                switch (hex[hex.Length - i - 1])
+                {
+                    case 'A': number = 10; break;
+                    case 'B': number = 11; break;
+                    case 'C': number = 12; break;
+                    case 'D': number = 13; break;
+                    case 'E': number = 14; break;
+                    case 'F': number = 15; break;
+                    default: number = hex[hex.Length - i - 1] - '0'; break;
+                }
+                dec += number * (long)Math.Pow(16, i);
+            }
+            Console.WriteLine(dec);
         }
     }
 }
