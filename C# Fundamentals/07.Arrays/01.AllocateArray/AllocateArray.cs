377ceffe4bacf62848faf8dfe1b299d5f5f3eafb
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-13 18:20:14 +0200

    Arrays

---
+++ C# Fundamentals/07.Arrays/01.AllocateArray/AllocateArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.AllocateArray
+{
+    class AllocateArray
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * 5;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
