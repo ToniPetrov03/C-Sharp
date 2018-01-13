1643d81075b3f06730b0b5f9b60a42542f3770ad
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-13 17:49:04 +0200

    Spiral matrix

--- C# Fundamentals/06.Loops/17.SpiralMatrix/SpiralMatrix.cs
+++ C# Fundamentals/06.Loops/17.SpiralMatrix/SpiralMatrix.cs
@@ -10,6 +10,64 @@ namespace _17.SpiralMatrix
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int startRow = 0;
+            int startCol = 0;
+            int endRow = n - 1;
+            int endCol = n - 1;
+            string direction = "R";
+            int a = 0;
+
+            while (endCol >= startCol && endRow >= startRow)
+            {
+                if (direction == "R")
+                {
+                    for (int c = startCol; c <= endCol; c++)
+                    {
+                        matrix[startRow, c] = ++a;
+                    }
+                    startRow++;
+                    direction = "D";
+                }
+                if (direction == "D")
+                {
+                    for (int r = startRow; r <= endRow; r++)
+                    {
+                        matrix[r, endCol] = ++a;
+                    }
+                    endCol--;
+                    direction = "L";
+                }
+                if (direction == "L")
+                {
+                    for (int c = endCol; c >= startCol; c--)
+                    {
+                        matrix[endRow, c] = ++a;
+                    }
+                    endRow--;
+                    direction = "U";
+                }
+                if (direction == "U")
+                {
+                    for (int r = endRow; r >= startRow; r--)
+                    {
+                        matrix[r, startCol] = ++a;
+                    }
+                    startCol++;
+                    direction = "R";
+                }
+
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
