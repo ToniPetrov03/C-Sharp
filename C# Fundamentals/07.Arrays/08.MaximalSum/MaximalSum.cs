d454a7b47810447a6d26c4dc6b502cd4d681a78b
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-14 17:29:16 +0200

    _

--- C# Fundamentals/07.Arrays/08.MaximalSum/MaximalSum.cs
+++ C# Fundamentals/07.Arrays/08.MaximalSum/MaximalSum.cs
@@ -10,6 +10,30 @@ namespace _08.MaximalSum
     {
         static void Main(string[] args)
         {
+
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = int.MinValue;
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = arr[i];
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    sum += arr[j];
+                    if (sum > max)
+                    {
+                        max = sum;
+                    }
+                }
+                sum = 0;
+            }
+            Console.WriteLine(max);
         }
     }
 }
