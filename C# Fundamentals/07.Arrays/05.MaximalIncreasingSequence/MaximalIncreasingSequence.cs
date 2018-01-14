51a1b2d5c8ed4204cf0febd809814a1a1c26bb36
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-14 14:54:23 +0200

    _

--- C# Fundamentals/07.Arrays/05.MaximalIncreasingSequence/MaximalIncreasingSequence.cs
+++ C# Fundamentals/07.Arrays/05.MaximalIncreasingSequence/MaximalIncreasingSequence.cs
@@ -10,6 +10,31 @@ namespace _05.MaximalIncreasingSequence
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            int result = 0;
+            int repeated = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > arr[i - 1])
+                {
+                    repeated++;
+                }
+                else
+                {
+                    if (repeated > result)
+                    {
+                        result = repeated;
+                    }
+                    repeated = 1;
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
--- C# Fundamentals/07.Arrays/06.MaximalKSum/MaximalKSum.cs
+++ C# Fundamentals/07.Arrays/06.MaximalKSum/MaximalKSum.cs
@@ -10,6 +10,21 @@ namespace _06.MaximalKSum
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            int result = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            for (int i = arr.Length - 1; i >= arr.Length - k; i--)
+            {
+                result += arr[i];
+            }
+            Console.WriteLine(result);
         }
     }
 }
