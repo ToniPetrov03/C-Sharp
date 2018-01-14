ed0153689b932b57eeccf36750dcf654d7351a84
ToniPetrov03 <toniivanov03@gmail.com>
2018-01-14 15:00:47 +0200

    _

--- C# Fundamentals/07.Arrays/07.SelectionSort/SelectionSort.cs
+++ C# Fundamentals/07.Arrays/07.SelectionSort/SelectionSort.cs
@@ -10,6 +10,19 @@ namespace _07.SelectionSort
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
         }
     }
 }
