480d9d42af45db89985e9fdd90b143f8438012b3
ToniPetrov03 <toniivanov03@gmail.com>
2018-02-10 21:17:34 +0200

    _

--- C# Fundamentals/07.Arrays/08.MaximalSum/MaximalSum.cs
+++ C# Fundamentals/07.Arrays/08.MaximalSum/MaximalSum.cs
@@ -10,30 +10,98 @@ namespace _08.MaximalSum
     {
         static void Main(string[] args)
         {
+            int a = NashParse(Console.ReadLine());
+            Console.WriteLine(a);
+            int b = NashParse(Console.ReadLine());
+            int c = NashParse(Console.ReadLine());
+            int d = NashParse(Console.ReadLine());
+            int e = NashParse(Console.ReadLine());
 
-            int n = int.Parse(Console.ReadLine());
-            int sum = 0;
-            int max = int.MinValue;
-            int[] arr = new int[n];
+            int smallest = int.MaxValue;
 
-            for (int i = 0; i < n; i++)
+            int abc = LCM(a, LCM(b, c));
+            int abd = LCM(a, LCM(b, d));
+            int abe = LCM(a, LCM(b, e));
+            int acd = LCM(a, LCM(c, d));
+            int ace = LCM(a, LCM(c, e));
+            int ade = LCM(a, LCM(d, e));
+            int bcd = LCM(b, LCM(c, d));
+            int bce = LCM(b, LCM(c, e));
+            int bde = LCM(b, LCM(d, e));
+            int cde = LCM(c, LCM(d, e));
+
+            if (abc < smallest)
+                smallest = abc;
+
+            if (abd < smallest)
+                smallest = abd;
+
+            if (abe < smallest)
+                smallest = abe;
+
+            if (acd < smallest)
+                smallest = acd;
+
+            if (ace < smallest)
+                smallest = ace;
+
+            if (ade < smallest)
+                smallest = ade;
+
+            if (bcd < smallest)
+                smallest = bcd;
+
+            if (bce < smallest)
+                smallest = bce;
+
+            if (bde < smallest)
+                smallest = bde;
+
+            if (cde < smallest)
+                smallest = cde;
+
+            //Console.WriteLine(smallest);
+        }
+
+        private static int TehenParse(string a)
+        {
+            return int.Parse(a);
+        }
+
+        private static int NashParse(string a)
+        {
+            int number = 0;
+            int multiple = 1;
+            for (int i = 0; i < a.Length; i++)
             {
-                arr[i] = int.Parse(Console.ReadLine());
+                int digit = a[a.Length - 1 - i] - '0';
+                number += digit * multiple;
+                multiple *= 10;
             }
-            for (int i = 0; i < arr.Length; i++)
+            return number;
+        }
+        
+
+        private static int LCM(int a, int b)
+        {
+            int num1, num2;
+            if (a > b)
             {
-                sum = arr[i];
-                for (int j = i + 1; j < arr.Length; j++)
+                num1 = a; num2 = b;
+            }
+            else
+            {
+                num1 = b; num2 = a;
+            }
+
+            for (int i = 1; i < num2; i++)
+            {
+                if ((num1 * i) % num2 == 0)
                 {
-                    sum += arr[j];
-                    if (sum > max)
-                    {
-                        max = sum;
-                    }
+                    return i * num1;
                 }
-                sum = 0;
             }
-            Console.WriteLine(max);
+            return num1 * num2;
         }
     }
 }
