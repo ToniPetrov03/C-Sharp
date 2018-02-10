8f083ca57fc2a235e93295e014f561da1912d5ac
ToniPetrov03 <toniivanov03@gmail.com>
2018-02-10 22:30:21 +0200

    NashCommit

--- C# Fundamentals/07.Arrays/08.MaximalSum/NashMaximalSum.cs
+++ C# Fundamentals/07.Arrays/08.MaximalSum/NashMaximalSum.cs
@@ -1,182 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace _08.MaximalSum
 {
-    class NashMaximalSum
+    class MaximalSum
     {
         static void Main(string[] args)
         {
-            // nash code
-            NashInt nashoA = NashParse(Console.ReadLine());
-            NashInt nashoB = NashParse(Console.ReadLine());
-            NashInt nashoC = NashParse(Console.ReadLine());
-            NashInt nashoD = NashParse(Console.ReadLine());
-            NashInt nashoE = NashParse(Console.ReadLine());
 
-            NashInt NashSmallest = NashInt.NashoMaxValue;
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = int.MinValue;
+            int[] arr = new int[n];
 
-            NashInt nashoAbc = NashoLCM(nashoA, NashoLCM(nashoB, nashoC));
-            NashInt nashoAbd = NashoLCM(nashoA, NashoLCM(nashoB, nashoD));
-            NashInt nashoAbe = NashoLCM(nashoA, NashoLCM(nashoB, nashoE));
-            NashInt nashoAcd = NashoLCM(nashoA, NashoLCM(nashoC, nashoD));
-            NashInt nashoAce = NashoLCM(nashoA, NashoLCM(nashoC, nashoE));
-            NashInt nashoAde = NashoLCM(nashoA, NashoLCM(nashoD, nashoE));
-            NashInt nashoBcd = NashoLCM(nashoB, NashoLCM(nashoC, nashoD));
-            NashInt nashoBce = NashoLCM(nashoB, NashoLCM(nashoC, nashoE));
-            NashInt nashoBde = NashoLCM(nashoB, NashoLCM(nashoD, nashoE));
-            NashInt nashoCde = NashoLCM(nashoC, NashoLCM(nashoD, nashoE));
-
-            if (nashoAbc < NashSmallest)
-                NashSmallest = nashoAbc;
-
-            if (nashoAbd < NashSmallest)
-                NashSmallest = nashoAbd;
-
-            if (nashoAbe < NashSmallest)
-                NashSmallest = nashoAbe;
-
-            if (nashoAcd < NashSmallest)
-                NashSmallest = nashoAcd;
-
-            if (nashoAce < NashSmallest)
-                NashSmallest = nashoAce;
-
-            if (nashoAde < NashSmallest)
-                NashSmallest = nashoAde;
-
-            if (nashoBcd < NashSmallest)
-                NashSmallest = nashoBcd;
-
-            if (nashoBce < NashSmallest)
-                NashSmallest = nashoBce;
-
-            if (nashoBde < NashSmallest)
-                NashSmallest = nashoBde;
-
-            if (nashoCde < NashSmallest)
-                NashSmallest = nashoCde;
-
-            Console.WriteLine(NashSmallest);
-        }
-
-        private static NashInt NashParse(string nashoA)
-        {
-            NashInt nashNumber = 0;
-            NashInt nashMultiple = 1;
-
-            for (NashInt nashoI = 0; nashoI < nashoA.Length; nashoI++)
+            for (int i = 0; i < n; i++)
             {
-                NashInt nashDigit = nashoA[nashoA.Length - 1 - nashoI] - '0';
-                nashNumber += nashDigit * nashMultiple;
-                nashMultiple *= 10;
+                arr[i] = int.Parse(Console.ReadLine());
             }
-            return nashNumber;
-        }
-        
-
-        private static int NashoLCM(NashInt a, NashInt b)
-        {
-            NashInt num1, num2;
-            if (a > b)
+            for (int i = 0; i < arr.Length; i++)
             {
-                num1 = a; num2 = b;
-            }
-            else
-            {
-                num1 = b; num2 = a;
-            }
-
-            for (NashInt i = 1; i < num2; i++)
-            {
-                if ((num1 * i) % num2 == 0)
+                sum = arr[i];
+                for (int j = i + 1; j < arr.Length; j++)
                 {
-                    return i * num1;
+                    sum += arr[j];
+                    if (sum > max)
+                    {
+                        max = sum;
+                    }
                 }
+                sum = 0;
             }
-            return num1 * num2;
+            Console.WriteLine(max);
+
         }
-
-        public class NashInt : CustomValueType<NashInt, Int32>
-        {
-            private NashInt(NashInt value) : base(value) { }
-            public const Int32 NashoMaxValue = 2147483647;
-            public static implicit operator NashInt(int value) { return new NashInt(value); }
-            public static implicit operator int(NashInt custom) { return custom._value; }
-        }
-
-        public class CustomValueType<TCustom, TValue>
-        {
-            protected readonly TValue _value;
-
-            public CustomValueType(TValue value)
-            {
-                _value = value;
-            }
-
-            public override string ToString()
-            {
-                return _value.ToString();
-            }
-
-            public static bool operator <(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
-            {
-                return Comparer<TValue>.Default.Compare(a._value, b._value) < 0;
-            }
-
-            public static bool operator >(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
-            {
-                return !(a < b);
-            }
-
-            public static bool operator <=(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
-            {
-                return (a < b) || (a == b);
-            }
-
-            public static bool operator >=(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
-            {
-                return (a > b) || (a == b);
-            }
-
-            public static bool operator ==(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
-            {
-                return a.Equals((object)b);
-            }
-
-            public static bool operator !=(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
-            {
-                return !(a == b);
-            }
-
-            public static TCustom operator +(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
-            {
-                return (dynamic)a._value + b._value;
-            }
-
-            public static TCustom operator -(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
-            {
-                return ((dynamic)a._value - b._value);
-            }
-
-            protected bool Equals(CustomValueType<TCustom, TValue> other)
-            {
-                return EqualityComparer<TValue>.Default.Equals(_value, other._value);
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (ReferenceEquals(null, obj)) return false;
-                if (ReferenceEquals(this, obj)) return true;
-                if (obj.GetType() != this.GetType()) return false;
-                return Equals((CustomValueType<TCustom, TValue>)obj);
-            }
-
-            public override int GetHashCode()
-            {
-                return EqualityComparer<TValue>.Default.GetHashCode(_value);
-            }
-        }
-
     }
 }
--- C# Fundamentals/08.Exams/02.EzxamMarch2012/02.LeastMajorityMultiple/LeastMajorityMultiple.cs
+++ C# Fundamentals/08.Exams/02.EzxamMarch2012/02.LeastMajorityMultiple/LeastMajorityMultiple.cs
@@ -1,39 +1,182 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _02.LeastMajorityMultiple
+namespace _08.MaximalSum
 {
-    class LeastMajorityMultiple
+    class NashMaximalSum
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[5];
-            int three = 0;
+            // nash code
+            NashInt nashoA = NashParse(Console.ReadLine());
+            NashInt nashoB = NashParse(Console.ReadLine());
+            NashInt nashoC = NashParse(Console.ReadLine());
+            NashInt nashoD = NashParse(Console.ReadLine());
+            NashInt nashoE = NashParse(Console.ReadLine());
 
-            for (int i = 0; i < 5; i++)
+            NashInt NashSmallest = NashInt.NashoMaxValue;
+
+            NashInt nashoAbc = NashoLCM(nashoA, NashoLCM(nashoB, nashoC));
+            NashInt nashoAbd = NashoLCM(nashoA, NashoLCM(nashoB, nashoD));
+            NashInt nashoAbe = NashoLCM(nashoA, NashoLCM(nashoB, nashoE));
+            NashInt nashoAcd = NashoLCM(nashoA, NashoLCM(nashoC, nashoD));
+            NashInt nashoAce = NashoLCM(nashoA, NashoLCM(nashoC, nashoE));
+            NashInt nashoAde = NashoLCM(nashoA, NashoLCM(nashoD, nashoE));
+            NashInt nashoBcd = NashoLCM(nashoB, NashoLCM(nashoC, nashoD));
+            NashInt nashoBce = NashoLCM(nashoB, NashoLCM(nashoC, nashoE));
+            NashInt nashoBde = NashoLCM(nashoB, NashoLCM(nashoD, nashoE));
+            NashInt nashoCde = NashoLCM(nashoC, NashoLCM(nashoD, nashoE));
+
+            if (nashoAbc < NashSmallest)
+                NashSmallest = nashoAbc;
+
+            if (nashoAbd < NashSmallest)
+                NashSmallest = nashoAbd;
+
+            if (nashoAbe < NashSmallest)
+                NashSmallest = nashoAbe;
+
+            if (nashoAcd < NashSmallest)
+                NashSmallest = nashoAcd;
+
+            if (nashoAce < NashSmallest)
+                NashSmallest = nashoAce;
+
+            if (nashoAde < NashSmallest)
+                NashSmallest = nashoAde;
+
+            if (nashoBcd < NashSmallest)
+                NashSmallest = nashoBcd;
+
+            if (nashoBce < NashSmallest)
+                NashSmallest = nashoBce;
+
+            if (nashoBde < NashSmallest)
+                NashSmallest = nashoBde;
+
+            if (nashoCde < NashSmallest)
+                NashSmallest = nashoCde;
+
+            Console.WriteLine(NashSmallest);
+        }
+
+        private static NashInt NashParse(string nashoA)
+        {
+            NashInt nashNumber = 0;
+            NashInt nashMultiple = 1;
+
+            for (NashInt nashoI = 0; nashoI < nashoA.Length; nashoI++)
             {
-                arr[i] = int.Parse(Console.ReadLine());
+                NashInt nashDigit = nashoA[nashoA.Length - 1 - nashoI] - '0';
+                nashNumber += nashDigit * nashMultiple;
+                nashMultiple *= 10;
+            }
+            return nashNumber;
+        }
+
+
+        private static int NashoLCM(NashInt a, NashInt b)
+        {
+            NashInt num1, num2;
+            if (a > b)
+            {
+                num1 = a; num2 = b;
+            }
+            else
+            {
+                num1 = b; num2 = a;
             }
 
-            for (int i = 1; i < 100000; i++)
+            for (NashInt i = 1; i < num2; i++)
             {
-                for (int j = 0; j < 5; j++)
+                if ((num1 * i) % num2 == 0)
                 {
-                    if (i >= arr[j] && i % arr[j] == 0)
-                    {
-                        three++;
-                        if (three >= 3)
-                        {
-                            Console.WriteLine(i);
-                            Environment.Exit(0);
-                        }
-                    }
+                    return i * num1;
                 }
-                three = 0;
+            }
+            return num1 * num2;
+        }
+
+        public class NashInt : CustomValueType<NashInt, Int32>
+        {
+            private NashInt(int value) : base(value) { }
+            public const Int32 NashoMaxValue = 2147483647;
+            public static implicit operator NashInt(int value) { return new NashInt(value); }
+            public static implicit operator int(NashInt custom) { return custom._value; }
+        }
+
+        public class CustomValueType<TCustom, TValue>
+        {
+            protected readonly TValue _value;
+
+            public CustomValueType(TValue value)
+            {
+                _value = value;
+            }
+
+            public override string ToString()
+            {
+                return _value.ToString();
+            }
+
+            public static bool operator <(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+            {
+                return Comparer<TValue>.Default.Compare(a._value, b._value) < 0;
+            }
+
+            public static bool operator >(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+            {
+                return !(a < b);
+            }
+
+            public static bool operator <=(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+            {
+                return (a < b) || (a == b);
+            }
+
+            public static bool operator >=(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+            {
+                return (a > b) || (a == b);
+            }
+
+            public static bool operator ==(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+            {
+                return a.Equals((object)b);
+            }
+
+            public static bool operator !=(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+            {
+                return !(a == b);
+            }
+
+            public static TCustom operator +(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+            {
+                return (dynamic)a._value + b._value;
+            }
+
+            public static TCustom operator -(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+            {
+                return ((dynamic)a._value - b._value);
+            }
+
+            protected bool Equals(CustomValueType<TCustom, TValue> other)
+            {
+                return EqualityComparer<TValue>.Default.Equals(_value, other._value);
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((CustomValueType<TCustom, TValue>)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return EqualityComparer<TValue>.Default.GetHashCode(_value);
             }
         }
+
     }
 }
