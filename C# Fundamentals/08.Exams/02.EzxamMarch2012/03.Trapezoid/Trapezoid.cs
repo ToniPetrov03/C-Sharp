3143c3e1d0f66c687e00cfff05bcc4c4d81e8a16
ToniPetrov03 <toniivanov03@gmail.com>
2018-02-11 14:25:38 +0200

    _

--- C# Fundamentals/08.Exams/02.EzxamMarch2012/03.Trapezoid/Trapezoid.cs
+++ C# Fundamentals/08.Exams/02.EzxamMarch2012/03.Trapezoid/Trapezoid.cs
@@ -10,6 +10,35 @@ namespace _03.Trapezoid
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int numberOfLeftDots = n - 1;
+            int numberOfRightDots = 0;
+            int numberOfDots = n - 1;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write('.');
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                Console.Write(new string('.', numberOfLeftDots));
+                Console.Write('*');
+                Console.Write(new string('.', numberOfRightDots));
+                Console.Write(new string('.', numberOfDots));
+                Console.Write('*');
+                numberOfLeftDots--;
+                numberOfRightDots++;
+                Console.WriteLine();
+            }
+            for (int i = 0; i < n*2; i++)
+            {
+                Console.Write('*');
+            }
         }
     }
 }
--- C# Fundamentals/08.Exams/02.EzxamMarch2012/04.OddNumber/OddNumber.cs
+++ C# Fundamentals/08.Exams/02.EzxamMarch2012/04.OddNumber/OddNumber.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _04.OddNumber
 {
@@ -10,6 +8,28 @@ namespace _04.OddNumber
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<long, int> dict = new Dictionary<long, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                long number = long.Parse(Console.ReadLine());
+                if (!dict.ContainsKey(number))
+                {
+                    dict.Add(number, 0);
+                }
+                dict[number]++;
+            }
+
+            foreach (var kv in dict)
+            {
+                if (kv.Value % 2 == 1)
+                {
+                    var myKey = dict.FirstOrDefault(x => x.Value == kv.Value).Key;
+                    Console.WriteLine(myKey);
+                    break;
+                }
+            }
         }
     }
 }
