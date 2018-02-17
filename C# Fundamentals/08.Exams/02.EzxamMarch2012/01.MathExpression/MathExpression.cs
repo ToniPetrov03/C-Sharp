c64393f7045b8adced2866c9a7303ea1962e246b
ToniPetrov03 <toniivanov03@gmail.com>
2018-02-17 17:01:37 +0200

    _

--- C# Fundamentals/08.Exams/02.EzxamMarch2012/01.MathExpression/MathExpression.cs
+++ C# Fundamentals/08.Exams/02.EzxamMarch2012/01.MathExpression/MathExpression.cs
@@ -9,7 +9,7 @@ namespace _01.MathExpression
             double N = double.Parse(Console.ReadLine());
             double M = double.Parse(Console.ReadLine());
             double P = double.Parse(Console.ReadLine());
-            int x = (int) M;
+            int x = (int)M;
 
             Console.WriteLine("{0:F6}", (N * N + 1 / (M * P) + 1337) / (N - 128.523123123 * P) + Math.Sin(x % 180));
         }
---
+++ C# Fundamentals/08.Exams/03.Dec2011Morning/ConsoleApp1/ShipDamage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipDamage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sx1 = int.Parse(Console.ReadLine());
+            int Sy1 = int.Parse(Console.ReadLine());
+            int Sx2 = int.Parse(Console.ReadLine());
+            int Sy2 = int.Parse(Console.ReadLine());
+            int H = int.Parse(Console.ReadLine());
+            int Cx1 = int.Parse(Console.ReadLine());
+            int Cy1 = int.Parse(Console.ReadLine());
+            int Cx2 = int.Parse(Console.ReadLine());
+            int Cy2 = int.Parse(Console.ReadLine());
+            int Cx3 = int.Parse(Console.ReadLine());
+            int Cy3 = int.Parse(Console.ReadLine());
+
+            Sy1 = H - (Sy1 - H);
+            Sy2 = H - (Sy2 - H);
+
+            int top = Math.Max(Sy1, Sy2);
+            int bottom = Math.Min(Sy1, Sy2);
+            int left = Math.Min(Sx1, Sx2);
+            int right = Math.Max(Sx1, Sx2);
+
+            int score1 = GetScore(Cx1, Cy1, top, bottom, left, right);
+            int score2 = GetScore(Cx2, Cy2, top, bottom, left, right);
+            int score3 = GetScore(Cx3, Cy3, top, bottom, left, right);
+
+            int score = score1 + score2 + score3;
+            Console.WriteLine(score + "%");
+        }
+
+        private static int GetScore(int x, int y, int top, int bottom, int left, int right)
+        {
+            int score = 0;
+            if (x < right && x > left && y > bottom && y < top)
+            {
+                score = 100;
+            }
+            // corner
+            else if (
+                x == right && y == bottom ||
+                x == left && y == top ||
+                x == right && y == top ||
+                x == left && y == bottom)
+            {
+                score = 25;
+            }
+            else if (
+                x == right && y < top && y > bottom ||
+                x == left && y < top && y > bottom ||
+                y == top && x > left && x < right ||
+                y == bottom && x > left && x < right)
+            {
+                score = 50;
+            }
+
+            return score;
+        }
+    }
+}
