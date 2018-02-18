54c4e275cb05a0fd4f16a80efceee2a8a9331cfd
ToniPetrov03 <toniivanov03@gmail.com>
2018-02-18 13:33:44 +0200

    _

---
+++ C# Fundamentals/08.Exams/03.Dec2011Morning/03.FirTree/FirTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.FirTree
+{
+    class FirTree
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int numberOfLeftDots = n - 2;
+            int numberOfRightAsterisk = 1;
+            int numberOfLeftAsterisk = 0;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                Console.Write(new string('.', numberOfLeftDots));
+                Console.Write(new string('*', numberOfRightAsterisk));
+                Console.Write(new string('*', numberOfLeftAsterisk));
+                Console.WriteLine(new string('.', numberOfLeftDots));
+
+                numberOfLeftDots--;
+                numberOfRightAsterisk++;
+                numberOfLeftAsterisk++;
+            }
+            Console.Write(new string('.', n - 2));
+            Console.Write('*');
+            Console.WriteLine(new string('.', n - 2));
+        }
+    }
+}
