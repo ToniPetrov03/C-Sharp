d68f144bbf0b20a6c7bd6cf8580241c87db8adb4
ToniPetrov03 <toniivanov03@gmail.com>
2018-02-18 12:54:38 +0200

    _

---
+++ C# Fundamentals/08.Exams/03.Dec2011Morning/02.Tribonacci/Tribonacci.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02.Tribonacci
+{
+    class Tribonacci
+    {
+        static void Main(string[] args)
+        {
+            BigInteger a = BigInteger.Parse(Console.ReadLine());
+            BigInteger b = BigInteger.Parse(Console.ReadLine());
+            BigInteger c = BigInteger.Parse(Console.ReadLine());
+            BigInteger n = BigInteger.Parse(Console.ReadLine());
+
+            if (n==1)
+            {
+                Console.WriteLine(a);
+            }
+            else if (n==2)
+            {
+                Console.WriteLine(b);
+            }
+            else if (n==3)
+            {
+                Console.WriteLine(c);
+            }
+            else if (n>3)
+            {
+                for (int i = 3; i < n; i++)
+                {
+                    BigInteger x = a;
+                    a = b;
+                    b = c;
+                    c = x + a + b;
+                }
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
