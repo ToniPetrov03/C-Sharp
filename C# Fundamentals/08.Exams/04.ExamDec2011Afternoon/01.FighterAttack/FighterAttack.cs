3b245f3a6d15208cf72e3b30d1365090ca15abdd
ToniPetrov03 <toniivanov03@gmail.com>
2018-03-03 18:35:30 +0200

    _

---
+++ C# Fundamentals/08.Exams/04.ExamDec2011Afternoon/01.FighterAttack/FighterAttack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01.FighterAttack
+{
+    class FighterAttack
+    {
+        static void Main(string[] args)
+        {
+            int Px1 = int.Parse(Console.ReadLine());
+            int Py1 = int.Parse(Console.ReadLine());
+            int Px2 = int.Parse(Console.ReadLine());
+            int Py2 = int.Parse(Console.ReadLine());
+            int Fx  = int.Parse(Console.ReadLine());
+            int Fy  = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+
+            int destruction = 0;
+
+
+            if (Fx + D >= Math.Min(Px1,Px2) && Fx + D <= Math.Max(Px1, Px2))
+            {
+                destruction = destruction + 100;
+            }
+            if (Fy + 1 <= Math.Max(Py1, Py2) && Fy + D >= Math.Min(Px1, Px2) && Fy + D <= Math.Max(Px1,Px2))
+            {
+                destruction = destruction + 50;
+            }
+            if (Fy - 1 >= Math.Min(Py1, Py2) && Fy + D >= Math.Min(Px1, Px2) && Fy + D <= Math.Max(Px1, Px2))
+            {
+                destruction = destruction + 50;
+            }
+            if (Fx + D + 1 <= Math.Max(Px1,Px2) && Fx + D + 1 >= Math.Min(Px1, Px2))
+            {
+                destruction = destruction + 75;
+            }
+            Console.WriteLine(destruction + "%");
+        }
+    }
+}
---
+++ C# Fundamentals/08.Exams/04.ExamDec2011Afternoon/02.AstrologicalDigits/AstrologicalDigits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.AstrologicalDigits
+{
+    class AstrologicalDigits
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            int result = 0;
+
+            n = Math.Abs(n);
+
+        }
+    }
+}
