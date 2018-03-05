0d5ed7c0bb26e844ec2b94f8269153f8f9bb7d84
ToniPetrov03 <toniivanov03@gmail.com>
2018-03-05 18:34:56 +0200

    _

---
+++ C# Fundamentals/08.Exams/05.Exam27Dec2012/01.NextDate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.NextDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            int mounth = int.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+
+            DateTime date = new DateTime(year, mounth, day);
+            date = date.AddDays(1);
+            Console.WriteLine(String.Format("{0:d.M.yyyy}", date));
+        }
+    }
+}
